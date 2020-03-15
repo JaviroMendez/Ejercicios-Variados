32d47699854cbee46379939a97b77c39495ed40b
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2020-03-14 19:33:47 -0600

    Add files via upload

---
+++ PilaLista/PilaLista/Lista.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace PilaLista
+{
+    partial class Lista
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnIralaLista = new System.Windows.Forms.Button();
+            this.Txt1 = new System.Windows.Forms.TextBox();
+            this.Txt2 = new System.Windows.Forms.TextBox();
+            this.Txt3 = new System.Windows.Forms.TextBox();
+            this.Txt4 = new System.Windows.Forms.TextBox();
+            this.Txt5 = new System.Windows.Forms.TextBox();
+            this.Txt6 = new System.Windows.Forms.TextBox();
+            this.Txt7 = new System.Windows.Forms.TextBox();
+            this.Txt8 = new System.Windows.Forms.TextBox();
+            this.Txt9 = new System.Windows.Forms.TextBox();
+            this.Txt10 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TxtValor = new System.Windows.Forms.TextBox();
+            this.LblValor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnIralaLista
+            // 
+            this.BtnIralaLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnIralaLista.Location = new System.Drawing.Point(109, 433);
+            this.BtnIralaLista.Name = "BtnIralaLista";
+            this.BtnIralaLista.Size = new System.Drawing.Size(92, 23);
+            this.BtnIralaLista.TabIndex = 29;
+            this.BtnIralaLista.Text = "Ir a la Pila";
+            this.BtnIralaLista.UseVisualStyleBackColor = true;
+            this.BtnIralaLista.Click += new System.EventHandler(this.BtnIralaLista_Click);
+            // 
+            // Txt1
+            // 
+            this.Txt1.Location = new System.Drawing.Point(133, 80);
+            this.Txt1.Multiline = true;
+            this.Txt1.Name = "Txt1";
+            this.Txt1.Size = new System.Drawing.Size(36, 36);
+            this.Txt1.TabIndex = 28;
+            this.Txt1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt2
+            // 
+            this.Txt2.Location = new System.Drawing.Point(133, 113);
+            this.Txt2.Multiline = true;
+            this.Txt2.Name = "Txt2";
+            this.Txt2.Size = new System.Drawing.Size(36, 36);
+            this.Txt2.TabIndex = 27;
+            this.Txt2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt3
+            // 
+            this.Txt3.Location = new System.Drawing.Point(133, 143);
+            this.Txt3.Multiline = true;
+            this.Txt3.Name = "Txt3";
+            this.Txt3.Size = new System.Drawing.Size(36, 36);
+            this.Txt3.TabIndex = 26;
+            this.Txt3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt4
+            // 
+            this.Txt4.Location = new System.Drawing.Point(133, 175);
+            this.Txt4.Multiline = true;
+            this.Txt4.Name = "Txt4";
+            this.Txt4.Size = new System.Drawing.Size(36, 36);
+            this.Txt4.TabIndex = 25;
+            this.Txt4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt5
+            // 
+            this.Txt5.Location = new System.Drawing.Point(133, 208);
+            this.Txt5.Multiline = true;
+            this.Txt5.Name = "Txt5";
+            this.Txt5.Size = new System.Drawing.Size(36, 36);
+            this.Txt5.TabIndex = 24;
+            this.Txt5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt6
+            // 
+            this.Txt6.Location = new System.Drawing.Point(133, 239);
+            this.Txt6.Multiline = true;
+            this.Txt6.Name = "Txt6";
+            this.Txt6.Size = new System.Drawing.Size(36, 36);
+            this.Txt6.TabIndex = 23;
+            this.Txt6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt7
+            // 
+            this.Txt7.Location = new System.Drawing.Point(133, 272);
+            this.Txt7.Multiline = true;
+            this.Txt7.Name = "Txt7";
+            this.Txt7.Size = new System.Drawing.Size(36, 36);
+            this.Txt7.TabIndex = 22;
+            this.Txt7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt8
+            // 
+            this.Txt8.Location = new System.Drawing.Point(133, 303);
+            this.Txt8.Multiline = true;
+            this.Txt8.Name = "Txt8";
+            this.Txt8.Size = new System.Drawing.Size(36, 36);
+            this.Txt8.TabIndex = 21;
+            this.Txt8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt9
+            // 
+            this.Txt9.Location = new System.Drawing.Point(133, 336);
+            this.Txt9.Multiline = true;
+            this.Txt9.Name = "Txt9";
+            this.Txt9.Size = new System.Drawing.Size(36, 36);
+            this.Txt9.TabIndex = 20;
+            this.Txt9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Txt10
+            // 
+            this.Txt10.Location = new System.Drawing.Point(133, 369);
+            this.Txt10.Multiline = true;
+            this.Txt10.Name = "Txt10";
+            this.Txt10.Size = new System.Drawing.Size(36, 36);
+            this.Txt10.TabIndex = 19;
+            this.Txt10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(231, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(231, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TxtValor
+            // 
+            this.TxtValor.Location = new System.Drawing.Point(109, 12);
+            this.TxtValor.Name = "TxtValor";
+            this.TxtValor.Size = new System.Drawing.Size(100, 20);
+            this.TxtValor.TabIndex = 16;
+            this.TxtValor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtValor_KeyPress);
+            // 
+            // LblValor
+            // 
+            this.LblValor.AutoSize = true;
+            this.LblValor.Location = new System.Drawing.Point(27, 19);
+            this.LblValor.Name = "LblValor";
+            this.LblValor.Size = new System.Drawing.Size(76, 13);
+            this.LblValor.TabIndex = 15;
+            this.LblValor.Text = "Dame el Valor:";
+            // 
+            // Lista
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 480);
+            this.Controls.Add(this.BtnIralaLista);
+            this.Controls.Add(this.Txt1);
+            this.Controls.Add(this.Txt2);
+            this.Controls.Add(this.Txt3);
+            this.Controls.Add(this.Txt4);
+            this.Controls.Add(this.Txt5);
+            this.Controls.Add(this.Txt6);
+            this.Controls.Add(this.Txt7);
+            this.Controls.Add(this.Txt8);
+            this.Controls.Add(this.Txt9);
+            this.Controls.Add(this.Txt10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtValor);
+            this.Controls.Add(this.LblValor);
+            this.Name = "Lista";
+            this.Text = "Lista";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnIralaLista;
+        private System.Windows.Forms.TextBox Txt1;
+        private System.Windows.Forms.TextBox Txt2;
+        private System.Windows.Forms.TextBox Txt3;
+        private System.Windows.Forms.TextBox Txt4;
+        private System.Windows.Forms.TextBox Txt5;
+        private System.Windows.Forms.TextBox Txt6;
+        private System.Windows.Forms.TextBox Txt7;
+        private System.Windows.Forms.TextBox Txt8;
+        private System.Windows.Forms.TextBox Txt9;
+        private System.Windows.Forms.TextBox Txt10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TxtValor;
+        private System.Windows.Forms.Label LblValor;
+    }
+}
---
+++ PilaLista/PilaLista/Lista.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PilaLista
+{
+    public partial class Lista : Form
+    {
+        public Lista()
+        {
+            InitializeComponent();
+        }
+        int cont = 0;
+        int n;
+        private void TxtValor_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Validacion v = new Validacion();
+            v.SinLetras(e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                n = int.Parse(TxtValor.Text);
+                switch (cont + 1)
+                {
+                    case 1:
+                        Txt1.Text = "" + n;
+                        Txt1.BackColor = Color.Blue;
+                        cont++;
+                        break;
+                    case 2:
+                        Txt2.Text = "" + n;
+                        Txt2.BackColor = Color.Red;
+                        cont++;
+                        break;
+                    case 3:
+                        Txt3.Text = "" + n;
+                        Txt3.BackColor = Color.Yellow;
+                        cont++;
+                        break;
+                    case 4:
+                        Txt4.Text = "" + n;
+                        Txt4.BackColor = Color.Brown;
+                        cont++;
+                        break;
+                    case 5:
+                        Txt5.Text = "" + n;
+                        Txt5.BackColor = Color.Green;
+                        cont++;
+                        break;
+                    case 6:
+                        Txt6.Text = "" + n;
+                        Txt6.BackColor = Color.Orange;
+                        cont++;
+                        break;
+                    case 7:
+                        Txt7.Text = "" + n;
+                        Txt7.BackColor = Color.Gray;
+                        cont++;
+                        break;
+                    case 8:
+                        Txt8.Text = "" + n;
+                        Txt8.BackColor = Color.Violet;
+                        cont++;
+                        break;
+                    case 9:
+                        Txt9.Text = "" + n;
+                        Txt9.BackColor = Color.Gold;
+                        cont++;
+                        break;
+                    case 10:
+                        Txt10.Text = "" + n;
+                        Txt10.BackColor = Color.Fuchsia;
+                        cont++;
+                        break;
+                    default:
+                        MessageBox.Show("No se pueden agregar mas");
+                        cont = 11;
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cont > 10)
+                {
+                    cont = 10;
+                }
+                switch (cont)
+                {
+                    case 10:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = Txt5.Text;
+                        Txt4.BackColor = Txt5.BackColor;
+                        Txt5.Text = Txt6.Text;
+                        Txt5.BackColor = Txt6.BackColor;
+                        Txt6.Text = Txt7.Text;
+                        Txt6.BackColor = Txt7.BackColor;
+                        Txt7.Text = Txt8.Text;
+                        Txt7.BackColor = Txt8.BackColor;
+                        Txt8.Text = Txt9.Text;
+                        Txt8.BackColor = Txt9.BackColor;
+                        Txt9.Text = Txt10.Text;
+                        Txt9.BackColor = Txt10.BackColor;
+                        Txt10.Text = "";
+                        Txt10.BackColor = Color.Empty;
+                        break;
+                    case 9:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = Txt5.Text;
+                        Txt4.BackColor = Txt5.BackColor;
+                        Txt5.Text = Txt6.Text;
+                        Txt5.BackColor = Txt6.BackColor;
+                        Txt6.Text = Txt7.Text;
+                        Txt6.BackColor = Txt7.BackColor;
+                        Txt7.Text = Txt8.Text;
+                        Txt7.BackColor = Txt8.BackColor;
+                        Txt8.Text = Txt9.Text;
+                        Txt8.BackColor = Txt9.BackColor;
+                        Txt9.Text = "";
+                        Txt9.BackColor = Color.Empty;
+                        break;
+                    case 8:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = Txt5.Text;
+                        Txt4.BackColor = Txt5.BackColor;
+                        Txt5.Text = Txt6.Text;
+                        Txt5.BackColor = Txt6.BackColor;
+                        Txt6.Text = Txt7.Text;
+                        Txt6.BackColor = Txt7.BackColor;
+                        Txt7.Text = Txt8.Text;
+                        Txt7.BackColor = Txt8.BackColor;
+                        Txt8.Text = "";
+                        Txt8.BackColor = Color.Empty;
+                        break;
+                    case 7:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = Txt5.Text;
+                        Txt4.BackColor = Txt5.BackColor;
+                        Txt5.Text = Txt6.Text;
+                        Txt5.BackColor = Txt6.BackColor;
+                        Txt6.Text = Txt7.Text;
+                        Txt6.BackColor = Txt7.BackColor;
+                        Txt7.Text = "";
+                        Txt7.BackColor = Color.Empty;
+                        break;
+                    case 6:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = Txt5.Text;
+                        Txt4.BackColor = Txt5.BackColor;
+                        Txt5.Text = Txt6.Text;
+                        Txt5.BackColor = Txt6.BackColor;
+                        Txt6.Text = "";
+                        Txt6.BackColor = Color.Empty;
+                        break;
+                    case 5:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = Txt5.Text;
+                        Txt4.BackColor = Txt5.BackColor;
+                        Txt5.Text = "";
+                        Txt5.BackColor = Color.Empty;
+                        break;
+                    case 4:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = Txt4.Text;
+                        Txt3.BackColor = Txt4.BackColor;
+                        Txt4.Text = "";
+                        Txt4.BackColor = Color.Empty;
+                        break;
+                    case 3:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = Txt3.Text;
+                        Txt2.BackColor = Txt3.BackColor;
+                        Txt3.Text = "";
+                        Txt3.BackColor = Color.Empty;
+                        break;
+                    case 2:
+                        Txt1.Text = Txt2.Text;
+                        Txt1.BackColor = Txt2.BackColor;
+                        Txt2.Text = "";
+                        Txt2.BackColor = Color.Empty;
+                        break;
+                    case 1:
+                        Txt1.Text = "";
+                        Txt1.BackColor = Color.Empty;
+                        break;
+                    default:
+                        MessageBox.Show("No se pueden quitar mas");
+                        cont = 1;
+                        break;
+                }
+                cont--;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void BtnIralaLista_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Pila p = new Pila();
+            p.Show();
+        }
+    }
+}
