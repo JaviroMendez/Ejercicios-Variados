667b290dd24f496e7cde67e7d171369460424a2d
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2020-03-14 19:34:24 -0600

    Add files via upload

---
+++ Tarea2/Tarea2/Bebida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea2
+{
+    class Bebida:Tienda
+    {
+        private string nombre;
+        private int precio;
+        public override void producto(string nombre)
+        {
+         this.nombre = nombre;
+        }
+        public override void valor(int precio)
+        {
+            this.precio = precio;
+        }
+        public override void mostrar()
+        {
+            Console.WriteLine("Hola soy una bebida llamada "+ nombre + " y mi precio es: "+ precio + " xD");
+        }
+    }
+}
---
+++ Tarea2/Tarea2/Botana.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace Tarea2
+{
+    class Botana : Tienda
+    {
+        private string nombre;
+        private int precio;
+        public override void producto(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public override void valor(int precio)
+        {
+            this.precio = precio;
+        }
+        public override void mostrar()
+        {
+            Console.WriteLine("Yo soy,mmmmmmmm "+ nombre +" cuesto: "+ precio + "adios");
+        }
+    }
+}
