d27caf3d128f6b8ed69eeb645fbfc3a75e0b4d5f
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2020-03-14 19:28:26 -0600

    Add files via upload

---
+++ ClaseAbstracta3/ClaseAbstracta3/Chivas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta3
+{
+    class Chivas : Equipo
+    {
+        private int Cantidad;
+        public override void campeonatos(int Cantidad)
+        {
+            this.Cantidad = Cantidad;
+        }
+        public override void mostrar()
+        {
+            Console.WriteLine("Las Chivas tienen: " + Cantidad + " campeonatos");
+        }
+    }
+}
---
+++ ClaseAbstracta3/ClaseAbstracta3/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta3
+{
+    public abstract class Equipo
+    {
+        public abstract void campeonatos(int campeonatos);
+        public abstract void mostrar();
+    }
+}
