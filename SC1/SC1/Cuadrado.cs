64088da626135850d22d015cde5f713cf5fab679
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2020-03-14 19:33:17 -0600

    Add files via upload

---
+++ SC1/SC1/Cuadrado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC1
+{
+    public class Cuadrado:Figura
+    {
+        public Cuadrado(double lado) : base(lado)
+        {
+            Console.WriteLine("El area del cuadrado es: "+(lado*lado));
+        }
+    }
+}
---
+++ SC1/SC1/Figura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC1
+{
+    public class Figura
+    {
+        protected double lado,b,altura;
+        public Figura(double lado)
+        {
+            this.lado = lado;
+        }
+        public Figura(double b, double altura)
+        {
+            this.b = b;
+            this.altura = altura;
+        }
+    }
+}
