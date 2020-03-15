8fc51100adffbf8496172ad34bee0af1e811fcea
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2020-03-14 19:28:21 -0600

    Add files via upload

---
+++ ClaseAbstracta/ClaseAbstracta/Celular.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta
+{
+    public abstract class Celular
+    {
+        public abstract void precio(double valor);
+        public abstract void color(string color);
+        public abstract void mostrar();
+    }
+}
---
+++ ClaseAbstracta/ClaseAbstracta/Nokia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta
+{
+    public class Nokia:Celular
+    {
+        private double valor;
+        private string color2;
+        public override void precio(double valor)
+        {
+            this.valor = valor;
+        }
+        public override void color(string color2)
+        {
+            this.color2 = color2;
+        }
+        public override void mostrar()
+        {
+            Console.WriteLine("El Celular Nokia cuesta: "+valor+ " y es de color: "+color2);
+        }
+    }
+}
