bb0cbcd0b6eee1c28eb3a910704ba0a7277d9d68
Javier Alejandro Mendez Rodriguez <36248601+JaviroMendez@users.noreply.github.com>
2020-03-14 19:28:22 -0600

    Add files via upload

---
+++ ClaseAbstracta2/ClaseAbstracta2/Danza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta2
+{
+    public abstract class Danza
+    {
+        public abstract void estado(string lugar);
+        public abstract void duracion(int tiempo);
+        public abstract void mostrar();
+    }
+}
---
+++ ClaseAbstracta2/ClaseAbstracta2/Kurpites.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta2
+{
+    class Kurpites:Danza
+    {
+        private string lugar;
+        private int tiempo;
+        public override void estado(string lugar)
+        {
+            this.lugar = lugar;
+        }
+        public override void duracion(int tiempo)
+        {
+            this.tiempo = tiempo;
+        }
+        public override void mostrar()
+        {
+            Console.WriteLine("Kurpites se baila en: " +lugar+ " y dura: " + tiempo+" minutos");
+        }
+    }
+}
